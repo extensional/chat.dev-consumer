c779f120e42b5ef9718262a1a45a26d1febe6b10
Nico Jones <jones.godel@gmail.com>
2023-03-03 14:47:25 -0800

    fix consumer deps

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export { IBotApi, IBotData, IBotDataModel, IBotDataParameter, ZodValidationResult, ZodErrorMapping } from "chat.dev-config";
+export {
+    IInteractionConsumerRequest,
+    IInteractionConsumerResponse,
+    IBotApi,
+    IBotData,
+    IBotDataModel,
+    IBotDataParameter,
+    ZodValidationResult,
+    ZodErrorMapping
+} from "chat.dev-config";
+
 export { Client as ChatDevClient } from "./client";
-export { InteractionRequest, InteractionResponse } from "./types/interaction.type";
