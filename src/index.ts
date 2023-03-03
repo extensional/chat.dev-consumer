6e92ee52b21366e8abf32de13416025ea43aa1e7
Nico Jones <jones.godel@gmail.com>
2023-03-02 19:04:07 -0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { IBotApi, IBotData, IBotDataParameter, ZodValidationResult, ZodErrorMapping } from "chat.dev-config";
 export { ChatDevClient } from "./client";
 export { InteractionRequest, InteractionResponse } from "./types/interaction.type";
+
+export const HELLO = "hello world";
+
+export const MYFUNC = () => { return "hello Myfunc" }
