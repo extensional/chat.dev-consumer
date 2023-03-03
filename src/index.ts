3c71d63ec4f65375c752af1cec35644f9c2ab7a7
Nico Jones <jones.godel@gmail.com>
2023-03-02 16:55:30 -0800

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export * from "./client";
+export { IBotApi, IBotData, IBotDataParameter, ZodValidationResult, ZodErrorMapping } from "chat.dev-config";
+export { ChatDevClient } from "./client";
+export { InteractionRequest, InteractionResponse } from "./types/interaction.type";
